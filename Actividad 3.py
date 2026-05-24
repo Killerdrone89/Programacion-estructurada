b0faa6e9c2fa59138aa623fda02024c27445eec7
Killerdrone89 <hassandiaz89@gmail.com>
2026-05-24 17:13:04 -0600

    Add files via upload

---
+++ Actividad 3.py
@@ -0,0 +1,46 @@
+print ("\033c")
+num_tabla=int(input("Dame un numero para obtener la tabla de multiplicar: "))
+
+num=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+multi=num_tabla*num
+print (f"{num_tabla} X {num} = {multi}")
+num+=1
+
+input ("Presione enter para terminar el programa: ")
